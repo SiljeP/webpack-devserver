862478526f90d5fc2f779e24c349d3a14856e2b1
SiljeP <u2440913@rts-365.dk>
2023-10-13 10:57:52 +0200

    initial commit with gitignore and without dist

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style.scss"
+import "./images/4c84bb42b56f399c7c2ccee9d422f1cd.png"
+import "./images/HarryStylesWembley170623_(65_of_93)_(52982678051)_(cropped_2).jpg"
+
+const headline = "welcome to the webpage"
+document.querySelector("h1").innerText = headline
